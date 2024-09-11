1a01000cdc971b3cce38c4c076ae886566e7750d
Brenek Harrison <brenekharrison@gmail.com>
2024-09-11 17:33:48 -0600

    Parse a file name and generate table from that json
    If no filename is passed, or the filename provided is `-`, standard input is used instead

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
+use clap::Parser;
+use std::path::PathBuf;
+
 use locatarr_json_to_readme::{generate_md_table, models::Applications};
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    /// Optional file to read in.
+    /// If no file is provided, or the file path is `-`, Standard Input will be used instead
+    json_file_path: Option<PathBuf>,
+}
+
 fn main() {
-    // TODO: Add clap for command line application usage
+    let cli = Cli::parse();
 
-    let json_string = "{\"applications\":[{\"name\":\"Sonarr\",\"description\":\"Automates & manages **TV series**\",\"github_slug\":\"Sonarr/Sonarr\",\"subreddit\":\"r/Sonarr\"}, {\"name\":\"Radarr\",\"description\":\"Automates & manages **Movies**\",\"github_slug\":\"Radarr/Radarr\",\"subreddit\":\"r/Radarr\"}]}";
+    let input_stream: Box<dyn std::io::Read + 'static> = match (
+        cli.json_file_path.clone(),
+        cli.json_file_path
+            .unwrap_or("".into())
+            .to_str()
+            .unwrap_or(""),
+    ) {
+        (Some(_), "-") | (None, _) => Box::new(std::io::stdin()),
+        (Some(file_path), _) => {
+            if let Ok(file) = std::fs::File::open(file_path) {
+                Box::new(file)
+            } else {
+                panic!("Could not read file");
+            }
+        }
+    };
 
-    let apps = serde_json::from_str::<Applications>(json_string).unwrap();
+    let apps_json: Applications = serde_json::from_reader(input_stream)
+        .expect("Could not parse JSON. Improper format detected");
 
-    println!("{}", generate_md_table(&apps));
+    let markdown_table = generate_md_table(&apps_json);
+
+    println!("{}", markdown_table);
 }
